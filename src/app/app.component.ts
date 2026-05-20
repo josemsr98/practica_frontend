4532112486ad1d750a3c26e4f822f432743c3635
josemsr98 <josemsr9398@gmail.com>
2026-05-20 17:55:43 +0200

    Añado cabecera de nombre de usuario

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,23 @@
 import { Component } from '@angular/core';
 import {RouterOutlet} from "@angular/router";
-
+import { obtenerUsuarioLogado } from './core/services/utils.service';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: "app-root",
   styleUrls: ['./app.component.css'],
   templateUrl: "./app.component.html",
   imports: [
-    RouterOutlet,
+    CommonModule,
+    RouterOutlet
   ],
   standalone: true,
 
 })
-export class AppComponent {
 
+export class AppComponent {
+  usuarioLogado: any = null;
+
+  constructor() {
+    this.usuarioLogado = obtenerUsuarioLogado();
+  }
 }
